e2c622bed2bf089b4b39a431902c2657e8f0d242
pjainz <pratjain@umich.edu>
2017-03-03 12:58:55 -0500

    Update Box.cpp

--- Box.cpp
+++ Box.cpp
@@ -1,25 +1,24 @@
 #include "Box.h"
 #include <iostream>
 
-Box::Box()
+Box::Box() // constructs box, which is the 3x3 pieces of the board
 	: box(3, vector<int>(3,0))
 {
 }
 
-Box::~Box() {
-}
+Box::~Box() {} // destructor
 
-void Box::writeNum(int num, int row, int col)
+void Box::writeNum(int num, int row, int col) // writes number in specified cell
 {
 	box[row][col] = num;
 }
 
-int Box::getNum(int row, int col)
+int Box::getNum(int row, int col)// gets number from specified cell
 {
 	return box[row][col];
 }
 
-void Box::print(int row)
+void Box::print(int row) // prints the box in console
 {
 	for(int j=0;j<3;j++)
 	{
@@ -27,7 +26,7 @@ void Box::print(int row)
 	}
 }
 
-vector<int> Box::numbersInBox()
+vector<int> Box::numbersInBox() // returns an array of numbers contained in the box
 {
 	vector<int> nums(9,0);
 	for(int i = 0; i<3; i++)
@@ -42,7 +41,7 @@ vector<int> Box::numbersInBox()
 	return nums;
 }
 
-vector<int> Box::numbersInCol(int colNum)
+vector<int> Box::numbersInCol(int colNum) // returns numbers in a specified column in a box
 {
 	vector<int> nums(9,0);
 	for (int i = 0; i<3; i++)
@@ -55,7 +54,7 @@ vector<int> Box::numbersInCol(int colNum)
 	return nums;
 }
 
-vector<int> Box::numbersInRow(int rowNum)
+vector<int> Box::numbersInRow(int rowNum)  // returns numbers in a specified row in a box
 {
 	vector<int> nums(9,0);
 	for (int j = 0; j<3; j++)
